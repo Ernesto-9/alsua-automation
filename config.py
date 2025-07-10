21acf8a66edc803284c0197686b0ce70dbf90325
Ernesto-9 <ernestouribe48@gmail.com>
2025-07-10 11:59:19 -0700

    Flask

--- config.py
+++
@@ -1,7 +0,0 @@
-# Configuración del sistema
-
-EMAIL_ACCOUNT = 'facturas@alsua.com.mx'
-MYSQL_HOST = 'localhost'
-MYSQL_USER = 'usuario'
-MYSQL_PASSWORD = 'contraseña'
-MYSQL_DATABASE = 'alsua_viajes'
