b5bdbf4328c1124b2fe991663dede2ae044655c4
Ernesto-9 <ernestouribe48@gmail.com>
2025-07-21 15:37:37 -0600

    borrado de cola

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, redirect
 import threading
 import time
 from alsua_mail_automation import AlsuaMailAutomation
-from modules.mysql_simple import mysql_acumulado
+from modules.mysql_simple import mysql_sync
 
-app = Flask(_name_)
+app = Flask(__name__)
 estado = {"ejecutando": False, "hilo": None}
 
 def ejecutar_bucle():
@@ -12,17 +12,17 @@ def ejecutar_bucle():
     try:
         sistema = AlsuaMailAutomation()
         print(">>> Instancia creada, ejecutando bucle continuo <<<")
-        sistema.ejecutar_bucle_continuo(intervalo_minutos=5)
+        sistema.ejecutar_bucle_continuo()
     except Exception as e:
         print(f"Error en ejecución del bucle: {e}")
 
 @app.route("/")
 def index():
     try:
-        if not mysql_acumulado.conectar():
+        if not mysql_sync.conectar():
             viajes = []
         else:
-            cursor = mysql_acumulado.connection.cursor(dictionary=True)
+            cursor = mysql_sync.connection.cursor(dictionary=True)
             cursor.execute("""
                 SELECT 
                     NOPREFACTURA as prefactura, 
@@ -62,5 +62,5 @@ def detener():
         # Solo detiene el flag, no mata el hilo de inmediato
     return redirect("/")
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0", port=5050)
